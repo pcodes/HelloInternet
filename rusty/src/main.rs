e75944a5a116eb52a430ecbcf324f31d8cf4306f
Pat Cody <patcody028@gmail.com>
2020-01-23 15:22:31 -0500

    Adds server code

--- rusty/src/main.rs
+++ rusty/src/main.rs
@@ -1,3 +1,42 @@
+/*
+Code modified from https://riptutorial.com/rust/example/4404/a-simple-tcp-client-and-server-application--echo
+*/
+
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50];
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occured, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+    println!("Server listening on port 80");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New Connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+    
+    drop(listener);
 }
