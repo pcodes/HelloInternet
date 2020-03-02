55c3650460b3e91984a9691ea04ae0e089d3ceaa
Pat Cody <patcody028@gmail.com>
2020-03-01 23:38:43 -0500

    Add commandline args and comments

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,10 +3,29 @@ Code modified from https://riptutorial.com/rust/example/4404/a-simple-tcp-client
 */
 
 use std::thread;
-use std::env;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::str::{from_utf8};
+use structopt::StructOpt;
+
+// The Opt struct serves as the command-line argument mapping. Research rust macros and structopt
+// for more details
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Hello Internet- Rust", about = "This serves as both the Rust client and server for Hello Internet.")]
+struct Opt {
+    #[structopt(long, required_unless("server"))]
+    client: bool,
+
+    #[structopt(long, required_unless("client"))]
+    server: bool, 
+
+    #[structopt(long, required_unless("server"))]
+    hostname: Option<String>,
+
+    #[structopt(long, required(true))]   
+    port: String,
+}
 
 fn handle_client(mut stream: TcpStream) {
     let response = "Goodbye in Rust!";
@@ -16,13 +35,18 @@ fn handle_client(mut stream: TcpStream) {
     stream.write(response.as_bytes()).unwrap();
 }
 
-fn server() {
-    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
-    println!("Server listening on port 8080");
+fn server(port: &str) {
+    let server_string = format!("0.0.0.0:{}", port);
+    let listener = TcpListener::bind(server_string).unwrap();
+
+    println!("Server listening on port {}", port);
+    
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("New Connection: {}", stream.peer_addr().unwrap());
+
+                // Create a new thread to handle new connection
                 thread::spawn(move || {
                     handle_client(stream)
                 });
@@ -36,8 +60,11 @@ fn server() {
     drop(listener);
 }
 
-fn client(){
-    match TcpStream::connect("localhost:8080") {
+fn client(port: &str, hostname: &str){
+    let client_string = format!("{}:{}", hostname, port);
+
+    // Match statement means either connection will succeed (and send data) or will fail to connect
+    match TcpStream::connect(client_string) {
         Ok(mut stream) => {
             println!("Sending \"Hello in Rust!\"");
 
@@ -50,12 +77,6 @@ fn client(){
                 Ok(size) => {
                     let text = from_utf8(&data[0..size]).unwrap();
                     println!("Received: {}", text);
-                    //if &data == msg {
-                    //    println!("Reply is ok!");
-                    //} else {
-                    //    let text = from_utf8(&data).unwrap();
-                    //    println!("Unexpected reply: {}", text);
-                    //}
                 },
                 Err(e) => {
                     println!("Failed to receive data: {}", e);
@@ -70,14 +91,12 @@ fn client(){
 }
 
 fn main() {
-    let is_server = env::args().any(|arg| arg == "--server");
-    let is_client = env::args().any(|arg| arg == "--client");
+    // get commandline args
+    let opt = Opt::from_args();
 
-    if is_server && !is_client {
-        server();
-    } else if !is_server && is_client {
-        client();
+    if opt.server {
+        server(&opt.port);
     } else {
-        println!("Please pass in either --client or --server");
+        client(&opt.port, &opt.hostname.unwrap());
     }
 }
